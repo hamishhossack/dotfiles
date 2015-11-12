5a2a4eaf50dfc26b0e9df6f5edd7cb012e24ab51
Hamish Hossack <hamishhossack@hotmail.co.uk>
2015-11-12 16:17:43 +0000

    Auto Enabled AutoSuggest

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,4 @@
-alias reload!='. ~/.zshrc'
 alias compile-ssh-config='cat ~/.ssh/config_local ~/.ssh/config_* > ~/.ssh/config'
 alias ssh='compile-ssh-config && ssh'
+alias ip='curl ipecho.net/plain ; echo'
+alias reload!='. ~/.zshrc'
