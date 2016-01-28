8fb3384afcebcc0e596c982f4c57b1017b0a5342
Hamish Hossack <hamishhossack@hotmail.co.uk>
2016-01-28 22:05:04 +0000

    Add simple-server to alias and add atom config

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,3 +2,4 @@ alias compile-ssh-config='cat ~/.ssh/config_local ~/.ssh/config_* > ~/.ssh/confi
 alias ssh='compile-ssh-config && ssh'
 alias ip='curl ipecho.net/plain ; echo'
 alias reload!='. ~/.zshrc'
+alias server='python -m SimpleHTTPServer 8000'
