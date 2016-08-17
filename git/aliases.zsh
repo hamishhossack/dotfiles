8ead5207423326eac0d63398902db5249a087f56
Hamish Hossack <hamish.hossack@essensys.tech>
2016-08-17 16:57:28 +0100

    new aliass for node + npm linting

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -12,6 +12,7 @@ alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C
 alias gp='git push origin HEAD'
 alias gd='git diff'
 alias gc='git commit'
+alias gcz='git cz'
 alias gca='git commit -a'
 alias gco='git checkout'
 alias gcb='git copy-branch-name'
