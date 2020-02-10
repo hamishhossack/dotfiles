aa83dbc91ad66ba46283103cd8078253fd1c466c
Hamish Hossack <hamishhossack@gmail.com>
2020-02-10 13:56:34 +0000

    refactor: remove smartfocus specifics

--- smartfocus/env.zsh
+++
@@ -1,6 +0,0 @@
-export PLUGIN_PATH=~/code/sf/tmc/container/plugins
-export CORE_PATH=~/code/sf/tmc/core
-
-# awsconsole
-source /Users/hamish.hossack/infastructure/sf-ops-tools/awsconsole/awsconsole-completion.sh
-alias awsconsole="/Users/hamish.hossack/infastructure/sf-ops-tools/awsconsole/awsconsole"
